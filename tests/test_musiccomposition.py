2b669492341ca92d564fc6ff27ea3139a37cc03d
pc2752 <pc2752@gmail.com>
2020-03-04 15:58:23 +0100

    Added music composition mutations and tests

---
+++ tests/test_musiccomposition.py
@@ -0,0 +1,73 @@
+# Tests for mutations pertaining to music composition objects.
+import os
+import unittest
+
+from trompace.mutations import musiccomposition
+from tests import util
+
+
+class TestDocument(unittest.TestCase):
+
+    def setUp(self) -> None:
+        super()
+        self.data_dir = os.path.join(os.path.dirname(__file__), "data", "musiccomposition")
+
+    def test_create(self):
+        expected = util.read_file(self.data_dir, "EXPECTED_MUSICCOMPOSITION.txt")
+
+        created_musiccomposition = musiccomposition.mutation_create_music_composition("A Document", "https://www.cpdl.org",
+                                                    "https://www.cpdl.org/A_Document", "https://www.upf.edu",
+                                                    "en", "en", name="A Document")
+        self.assertEqual(created_musiccomposition, expected)
+
+    def test_update(self):
+        expected = util.read_file(self.data_dir, "EXPECTED_MUSICCOMPOSITION_UPDATE.txt")
+
+        created_update = musiccomposition.mutation_update_music_composition('2eeca6dd-c62c-490e-beb0-2e3899fca74f',
+                                                  name="No Document")
+        self.assertEqual(created_update, expected)
+
+    def test_delete(self):
+        expected = util.read_file(self.data_dir, "EXPECTED_MUSICCOMPOSITION_DELETE.txt")
+
+        created_delete = musiccomposition.mutation_delete_music_composition('2eeca6dd-c62c-490e-beb0-2e3899fca74f')
+        self.assertEqual(created_delete, expected)
+
+    def test_add_broad_match(self):
+        expected = util.read_file(self.data_dir, "EXPECTED_MUSICCOMPOSITION_BROAD_MATCH.txt")
+
+        created_match = musiccomposition.mutation_add_broad_match_music_composition("ff562d2e-2265-4f61-b340-561c92e797e9",
+                                                          "59ce8093-5e0e-4d59-bfa6-805edb11e396")
+        self.assertEqual(created_match, expected)
+
+    def test_remove_broad_match(self):
+        expected = util.read_file(self.data_dir, "EXPECTED_MUSICCOMPOSITION_REMOVE_BROAD_MATCH.txt")
+
+        created_match = musiccomposition.mutation_remove_broad_match_music_composition(
+            "ff562d2e-2265-4f61-b340-561c92e797e9",
+            "59ce8093-5e0e-4d59-bfa6-805edb11e396") 
+        self.assertEqual(created_match, expected)
+
+    def test_add_exampleOf(self):
+        expected = util.read_file(self.data_dir, "EXPECTED_MUSICCOMPOSITION_ADD_EXAMPLEOF_COMPOSITION.txt")
+
+        created_match = musiccomposition.mutation_add_music_composition_work_example_composition(
+            "ff562d2e-2265-4f61-b340-561c92e797e9",
+            "59ce8093-5e0e-4d59-bfa6-805edb11e396")
+        self.assertEqual(created_match, expected)
+
+    def test_merge_exampleOf(self):
+        expected = util.read_file(self.data_dir, "EXPECTED_MUSICCOMPOSITION_MERGE_EXAMPLEOF_COMPOSITION.txt")
+
+        created_match = musiccomposition.mutation_merge_music_composition_work_example_composition(
+            "ff562d2e-2265-4f61-b340-561c92e797e9",
+            "59ce8093-5e0e-4d59-bfa6-805edb11e396")
+        self.assertEqual(created_match, expected)
+
+    def test_remove_exampleOf(self):
+        expected = util.read_file(self.data_dir, "EXPECTED_MUSICCOMPOSITION_REMOVE_EXAMPLEOF_COMPOSITION.txt")
+
+        created_match = musiccomposition.mutation_remove_music_composition_work_example_composition(
+            "ff562d2e-2265-4f61-b340-561c92e797e9",
+            "59ce8093-5e0e-4d59-bfa6-805edb11e396")
+        self.assertEqual(created_match, expected)
---
+++ trompace/mutations/musiccomposition.py
@@ -0,0 +1,275 @@
+# Generate GraphQL queries for mutations pertaining to music composition objects.
+from trompace.exceptions import UnsupportedLanguageException, NotAMimeTypeException
+from . import StringConstant
+from .templates import mutation_create, mutation_delete, mutation_link
+from ..constants import SUPPORTED_LANGUAGES
+
+ADD_MUSIC_COMPOSITION_BROAD_MATCH = '''AddMusicCompositionBroadMatch(
+    from: {{identifier: "{identifier_1}" }}
+    to: {{identifier: "{identifier_2}" }}
+  ) {{
+    from {{
+      identifier
+    }}
+    to {{
+      identifier
+    }}
+  }}'''
+
+REMOVE_MUSIC_COMPOSITION_BROAD_MATCH = '''RemoveMusicCompositionBroadMatch(
+    from: {{identifier: "{identifier_1}" }}
+    to: {{identifier: "{identifier_2}" }}
+  ) {{
+    from {{
+      identifier
+    }}
+    to {{
+      identifier
+    }}
+  }}'''
+
+ADD_MUSIC_COMPOSITION_WORK_EXAMPLE_COMPOSITION = '''AddDMusicCompositionExampleOfWork(
+    from: {{identifier: "{identifier_1}"}}
+    to: {{identifier: "{identifier_2}"}}
+) {{
+    from {{
+      identifier
+    }}
+    to {{
+      identifier
+    }}
+}}'''
+
+MERGE_MUSIC_COMPOSITION_WORK_EXAMPLE_COMPOSITION = '''MergeMusicCompositionExampleOfWork(
+    from: {{identifier: "{identifier_1}"}}
+    to: {{identifier: "{identifier_2}"}}
+) {{
+    from {{
+      identifier
+    }}
+    to {{
+      identifier
+    }}
+}}'''
+
+REMOVE_MUSIC_COMPOSITION_WORK_EXAMPLE_COMPOSITION = '''RemoveMusicCompositionExampleOfWork(
+    from: {{identifier: "{identifier_1}"}}
+    to: {{identifier: "{identifier_2}"}}
+) {{
+    from {{
+      identifier
+    }}
+    to {{
+      identifier
+    }}
+}}'''
+
+CREATE_MUSIC_COMPOSITION = '''CreateMusicComposition(
+        {parameters}
+  ) {{
+    identifier
+  }}'''
+
+UPDATE_MUSIC_COMPOSITION = '''UpdateMusicComposition(
+        {parameters}
+) {{
+  identifier
+}}'''
+
+DELETE_MUSIC_COMPOSITION = '''DeleteMusicComposition(
+    {parameters}
+  ) {{
+    identifier
+  }}'''
+
+ADD_MUSIC_COMPOSITION_TO_CONTROL_ACTION_MUTATION = """AddActionInterfaceThingInterface (
+            from: {{identifier: "{identifier_2}", type: ControlAction}}
+            to: {{identifier: "{identifier_1}", type: MusicComposition}}
+            field: result
+        ) {{
+            from {{
+                __typename
+            }}
+            to {{
+                __typename
+            }}
+        }}"""
+
+
+def mutation_create_music_composition(title: str, contributor: str, creator: str, source: str,
+                           language: str, inLanguage:str, formatin:str="text/html", name: str=None, description: str=None):
+    """Returns a mutation for creating a music composition object
+    Arguments:
+        title: The title of the page from which the music composition information was extracted.      
+        contributor: A person, an organization, or a service responsible for contributing the music composition to the web resource. This can be either a name or a base URL.
+        creator: The person, organization or service who created the thing the web resource is about.
+        source: The URL of the web resource to be represented by the node.
+        language: The language the metadata is written in. Currently supported languages are en,es,ca,nl,de,fr
+        inLanguage: The language of the music composition. Currently supported languages are en,es,ca,nl,de,fr
+        formatin: A MimeType of the format of the music composition, default is "text/html"
+        name: The name of the music composition.
+        description: An account of the music composition.
+
+
+    Returns:
+        The string for the mutation for creating the music composition.
+    Raises:
+        UnsupportedLanguageException if the input language or inLanguage is not one of the supported languages.
+    """
+    if language not in SUPPORTED_LANGUAGES:
+        raise UnsupportedLanguageException(language)
+
+    if inLanguage not in SUPPORTED_LANGUAGES:
+        raise UnsupportedLanguageException(inLanguage)
+
+    if "/" not in formatin:
+        raise NotAMimeTypeException(formatin)
+
+    args = {
+        "title": title,
+        "contributor": contributor,
+        "creator": creator,
+        "format": formatin,
+        "language": StringConstant(language.lower()),
+        "inLanguage": StringConstant(inLanguage.lower()),
+    }
+    if name:
+        args["name"] = name
+    if description:
+        args["description"] = description
+    return mutation_create(args, CREATE_MUSIC_COMPOSITION)
+
+
+def mutation_update_music_composition(identifier: str, title: str=None, contributor: str=None, creator: str=None, source: str=None,
+                           language: str=None, inLanguage:str=None, formatin:str=None, name: str=None, description: str=None):
+    """Returns a mutation for updating a music composition object.
+    Arguments:
+        title: The title of the page from which the music composition information was extracted.      
+        contributor: A person, an organization, or a service responsible for contributing the music composition to the web resource. This can be either a name or a base URL.
+        creator: The person, organization or service who created the thing the web resource is about.
+        source: The URL of the web resource to be represented by the node.
+        language: The language the metadata is written in. Currently supported languages are en,es,ca,nl,de,fr
+        inLanguage: The language of the music composition. Currently supported languages are en,es,ca,nl,de,fr
+        formatin: A MimeType of the format of the music composition, default is "text/html"
+        name: The name of the music composition.
+        description: An account of the music composition.
+    Returns:
+        The string for the mutation for updating the music composition.
+    Raises:
+        Assertion error if the input language or inLanguage is not one of the supported languages.
+    """
+        
+
+
+    args = {"identifier": identifier}
+    if title:
+        args["title"] = title
+    if contributor:
+        args["contributor"] = contributor
+    if creator:
+        args["creator"] = creator
+    if source:
+        args["source"] = source
+    if language:
+        if language not in SUPPORTED_LANGUAGES:
+            raise UnsupportedLanguageException(language)
+        else:
+            args["language"] = StringConstant(language.lower())
+    if inLanguage:
+        if inLanguage not in SUPPORTED_LANGUAGES:
+            raise UnsupportedLanguageException(inLanguage)
+        else:
+            args["inLanguage"] = StringConstant(inLanguage.lower())
+    if formatin:
+        args["format"] = formatin
+    if name:
+        args["name"] = name
+    if description:
+        args["description"] = description
+    return mutation_create(args, UPDATE_MUSIC_COMPOSITION)
+
+
+
+def mutation_delete_music_composition(identifier: str):
+    """Returns a mutation for deleting a music composition object based on the identifier.
+    Arguments:
+        identifier: The unique identifier of the music composition object.
+    Returns:
+        The string for the mutation for deleting the music composition object based on the identifier.
+    """
+
+    return mutation_delete(identifier, DELETE_MUSIC_COMPOSITION)
+
+
+def mutation_add_broad_match_music_composition(from_identifier: str, to_identifier: str):
+    """Returns a mutation for creating a broad match between two music comosition objects.
+    Arguments:
+        from_identifier: The unique identifier of the digital document object from which to create the broad match.
+        to_identifier: The unique identifier of the digital document object to which the broad match should be created.
+    Returns:
+        The string for the mutation for creating the broad match between the two documents.
+    """
+
+    return mutation_link(from_identifier, to_identifier, ADD_MUSIC_COMPOSITION_BROAD_MATCH)
+
+
+def mutation_remove_broad_match_music_composition(from_identifier: str, to_identifier: str):
+    """Returns a mutation for removing a broad match between two music composition objects.
+    Arguments:
+        from_identifier: The unique identifier of the music composition object from which to remove the broad match.
+        to_identifier: The unique identifier of the digital document object to which the broad match should be removed.
+    Returns:
+        The string for the mutation for removing the broad match between the music compositions.
+    """
+
+    return mutation_link(from_identifier, to_identifier, REMOVE_MUSIC_COMPOSITION_BROAD_MATCH)
+
+
+def mutation_add_music_composition_work_example_composition(music_composition_id: str, composition_id: str):
+    """Returns a mutation for adding a music composition as an example of a composition.
+    Arguments:
+        music_composition_id: The unique identifier of the music composition object.
+        composition_id: The unique identifier of the composition object.
+    Returns:
+        The string for the mutation for adding the music composition as an example of the composition.
+    """
+
+    return mutation_link(music_composition_id, composition_id, ADD_MUSIC_COMPOSITION_WORK_EXAMPLE_COMPOSITION)
+
+
+def mutation_merge_music_composition_work_example_composition(music_composition_id: str, composition_id: str):
+    """Returns a mutation for merging a music composition as an example of a composition.
+    Merging means that the connection will be added only if it does not exist.
+
+    Arguments:
+        music_composition_id: The unique identifier of the music composition object.
+        composition_id: The unique identifier of the composition object.
+    Returns:
+        The string for the mutation for merging the music composition as an example of the composition.
+    """
+
+    return mutation_link(music_composition_id, composition_id, MERGE_MUSIC_COMPOSITION_WORK_EXAMPLE_COMPOSITION)
+
+
+def mutation_remove_music_composition_work_example_composition(music_composition_id: str, composition_id: str):
+    """Returns a mutation for removing a music composition as an example of a composition.
+    Arguments:
+        music_composition_id: The unique identifier of the music composition object.
+        composition_id: The unique identifier of the composition object.
+    Returns:
+        The string for the mutation for removing the music composition as an example of the composition.
+    """
+
+    return mutation_link(music_composition_id, composition_id, REMOVE_MUSIC_COMPOSITION_WORK_EXAMPLE_COMPOSITION)
+
+
+def mutation_add_music_composition_controlaction(music_composition_id: str, controlaction_id: str):
+    """Returns a mutation for adding a music composition to a control action.
+    Arguments:
+        music_composition_id: The unique identifier of the music composition object.
+        composition_id: The unique identifier of the composition object.
+    Returns:
+        The string for the mutation for adding the music composition to the control action. 
+    """
+
+    return mutation_link(music_composition_id, controlaction_id, ADD_MUSIC_COMPOSITION_TO_CONTROL_ACTION_MUTATION)
