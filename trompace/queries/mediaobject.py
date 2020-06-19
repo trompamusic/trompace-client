1612e51907a0fb996937a71538123553eede5190
pc2752 <pc2752@gmail.com>
2020-06-19 16:30:09 +0200

    Trimmed argument list for person queries

--- trompace/queries/mediaobject.py
+++ trompace/queries/mediaobject.py
@@ -4,29 +4,22 @@ from trompace.queries.templates import format_query
 from trompace import StringConstant, _Neo4jDate, filter_none_args, docstring_interpolate
 from trompace.constants import SUPPORTED_LANGUAGES
 
-MEDIAOBJECT_ARGS_DOCS = """identifier: The identifier of the media object in the CE.
-        name: The name of the media object.
-        description: An account of the media object.
-        date: The date associated with the media object.
-        creator: The person, organization or service who created the thing the web resource is about.
-        contributor: A person, an organization, or a service responsible for contributing the media object to the web resource. This can be either a name or a base URL.
-        format_: A MimeType of the format of the page describing the media object.
-        encodingFormat: A MimeType of the format of object encoded by the media object.
-        source: The URL of the web resource to be represented by the node.
-        subject: The subject of the media object.
-        contentUrl: The URL of the content encoded by the media object.
-        language: The language the metadata is written in. Currently supported languages are en,es,ca,nl,de,fr
-        inLanguage: The language of the media object. Currently supported languages are en,es,ca,nl,de,fr
-        title: The title of the resource indicated by `source`"""
 
 
-@docstring_interpolate("mediaobject_args", MEDIAOBJECT_ARGS_DOCS)
 def query_mediaobject(identifier: str=None, creator: str=None, contributor: str=None,\
- encodingFormat: str=None, source: str=None, contentUrl:str=None, return_items_list: list=None):
+ encodingFormat: str=None, source: str=None, contentUrl:str=None,\
+  return_items_list: list=["identifier", "name"]):
 
     """Returns a query for querying the database for a media object.
     Arguments:
-        {mediaobject_args}
+        identifier: The identifier of the media object in the CE.
+        creator: The person, organization or service who created the thing the web resource is about.
+        contributor: A person, an organization, or a service responsible for contributing\
+         the media object to the web resource. This can be either a name or a base URL.
+        encodingFormat: A MimeType of the format of object encoded by the media object.
+        source: The URL of the web resource to be represented by the node.
+        contentUrl: The URL of the content encoded by the media object.
+        return_item_list: A list of item fields that the query must return.
     Returns:
         The string for the quereing the media object.
     Raises:
@@ -37,9 +30,7 @@ def query_mediaobject(identifier: str=None, creator: str=None, contributor: str=
 
     if encodingFormat and "/" not in encodingFormat:
         raise NotAMimeTypeException(encodingFormat)
-        
-    if not return_items_list:
-        return_items_list = ["identifier", "name"]
+
     args = {
         "identifier": identifier,
         "creator": creator,
--- trompace/queries/person.py
+++ trompace/queries/person.py
@@ -5,38 +5,20 @@ from trompace.queries.templates import format_query
 from trompace import StringConstant, _Neo4jDate, filter_none_args, docstring_interpolate
 from trompace.constants import SUPPORTED_LANGUAGES
 
-PERSON_ARGS_DOCS = """identifier: The identifier of the person in the CE.
-        title: The title of the resource indicated by `source`
-        contributor: The main URL of the site where the information about this Person was taken from
-        creator: The person, organization or service who is creating this Person (e.g. URL of the software)
-        source: The URL of the web resource where information about this Person is taken from
-        language: The language the metadata is written in. Currently supported languages are en,es,ca,nl,de,fr
-        format_: The mimetype of the resource indicated by `source`
-        name: The name of the person
-        family_name (optional): The family name of the person
-        given_name (optional): The given name of the person
-        gender (optional): The person's gender
-        birth_date (optional): The birth date of the person, formatted as yyyy, yyyy-mm or yyyy-mm-dd
-        death_date (optional): The date of death of the person , formatted as yyyy, yyyy-mm or yyyy-mm-dd
-        description (optional): A biographical description of the person
-        image (optional): URL to an image associated with the person
-        publisher (optional): An entity responsible for making the resource available
-        honorific_prefix (optional): An honorific prefix.
-        honorific_suffix (optional): An honorific suffix.
-        job_title (optional): The person's job title."""
 
-
-@docstring_interpolate("person_args", PERSON_ARGS_DOCS)
-def query_person(identifier: str=None,title: str=None, contributor: str=None, creator: str=None, source: str=None,
+def query_person(identifier: str=None, contributor: str=None, creator: str=None, source: str=None,
                            language: str = None, format_: str = None, name: str = None,
-                           family_name: str = None, given_name: str = None, gender: str = None,
-                           birth_date: str = None, death_date: str = None,
-                           description: str = None, image: str = None, publisher: str = None,
-                           honorific_prefix: str = None, honorific_suffix: str = None, job_title: str = None,\
-                            return_items_list: list=None):
+                           family_name: str = None, given_name: str = None,
+                            return_items_list: list=["identifier", "name"]):
     """Returns a query for querying the database for a person object.
     Arguments:
-        {person_args}
+        identifier: The identifier of the person in the CE.
+        contributor: The main URL of the site where the information about this Person was taken from
+        source: The URL of the web resource where information about this Person is taken from
+        name: The name of the person
+        family_name: The family name of the person
+        given_name: The given name of the person
+        return_item_list: A list of item fields that the query must return.
     Returns:
         The string for the mutation for creating the person.
     Raises:
@@ -44,38 +26,15 @@ def query_person(identifier: str=None,title: str=None, contributor: str=None, cr
         NotAMimeTypeException if `format_` is not a valid mimetype.
     """
 
-    if language and language.lower() not in SUPPORTED_LANGUAGES:
-        raise UnsupportedLanguageException(language)
-
-    if format_ and "/" not in format_:
-        raise NotAMimeTypeException(format_)
-    if not return_items_list:
-        return_items_list = ["identifier", "name"]
 
     args = {
         "identifier": identifier,
-        "title": title,
         "contributor": contributor,
-        "creator": creator,
         "source": source,
-        "format": format_,
         "name": name,
         "familyName": family_name,
         "givenName": given_name,
-        "gender": gender,
-        "description": description,
-        "image": image,
-        "publisher": publisher,
-        "honorificPrefix": honorific_prefix,
-        "honorificSuffix": honorific_suffix,
-        "jobTitle": job_title
     }
-    if language is not None:
-        args["language"] = StringConstant(language.lower())
-    if birth_date is not None:
-        args["birthDate"] = _Neo4jDate(birth_date)
-    if death_date is not None:
-        args["deathDate"] = _Neo4jDate(death_date)
 
     args = filter_none_args(args)
 
