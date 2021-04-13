d986ad5e01963a07ce13ca8d783dc9ea2e0e2bd4
Alastair Porter <alastair.porter@upf.edu>
2021-04-13 21:14:14 +0200

    Queries to get annotation toolkits, sessions, and annotations

--- trompace/queries/annotation.py
+++ trompace/queries/annotation.py
@@ -34,9 +34,84 @@ TOOLKIT_INCLUDES = """
 """
 
 
+SESSION_INCLUDES = """
+    identifier
+    name
+    creator
+    itemListElement {
+      ... on ListItem {
+        item {
+          ... on Annotation {
+            targetNode {
+              target {
+                __typename
+                identifier
+              }
+            }
+          }
+        }
+      }
+    }
+"""
+
+
+ANNOTATION_INCLUDES = """
+    targetNode {
+      target {
+        ... on AudioObject {
+          source
+        }
+      }
+    }
+    identifier
+    motivation
+    motivationNode {
+      identifier
+      broaderMotivation
+      title
+    }
+    bodyNode {
+      ... on Annotation {
+        identifier
+      }
+    }
+"""
+
+
 def get_annotation_toolkits_for_user(creator: str):
-    return format_query("ItemList", {"creator": creator}, TOOLKIT_INCLUDES)
+    return format_query(
+        "ItemList",
+        {"creator": creator,
+         "filter": {
+             "additionalType_contains": "https://vocab.trompamusic.eu/vocab#AnnotationToolkit"}
+         },
+        TOOLKIT_INCLUDES)
 
 
 def get_annotation_toolkit(identifier: str):
     return format_query("ItemList", {"identifier": identifier}, TOOLKIT_INCLUDES)
+
+
+def get_session_for_user(creator: str):
+    return format_query(
+        "ItemList",
+        {"creator": creator,
+         "filter": {
+             "additionalType_contains": "https://vocab.trompamusic.eu/vocab#AnnotationSession"}
+         },
+        SESSION_INCLUDES)
+
+
+def get_annotation(identifier: str):
+    return format_query("Annotation", {"identifier": identifier}, ANNOTATION_INCLUDES)
+
+
+def get_annotations_for_target_node(identifier: str):
+    """Get all annotations whose target node is given by the identifier"""
+    return format_query("Annotation",
+                        {"filter": {"targetNode": {"target_in": {"identifier": identifier}}}},
+                        ANNOTATION_INCLUDES)
+
+
+def get_annotations_for_user(creator: str):
+    return format_query("Annotation", {"creator": creator}, ANNOTATION_INCLUDES)
