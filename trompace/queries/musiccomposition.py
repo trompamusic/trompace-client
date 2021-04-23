f8fbd47b726d5f3da530791c5ccb8f4f5793ed64
Alastair Porter <alastair.porter@upf.edu>
2021-04-23 16:27:18 +0200

    improve musiccomposition queries

--- trompace/queries/musiccomposition.py
+++ trompace/queries/musiccomposition.py
@@ -1,13 +1,15 @@
 # Generate GraphQL queries for queries pertaining to music compositions.
+from typing import Optional, Union
+
 from trompace.exceptions import UnsupportedLanguageException, NotAMimeTypeException
 from trompace.queries.templates import format_query
-from trompace import StringConstant, _Neo4jDate, filter_none_args, docstring_interpolate
+from trompace import StringConstant, _Neo4jDate, filter_none_args, docstring_interpolate, make_filter
 from trompace.constants import SUPPORTED_LANGUAGES
 
 
 def query_musiccomposition(identifier: str = None, title: str = None, contributor: str = None, creator: str = None,
                            source: str = None, inlanguage: str = None, name: str = None, position: int = None,
-                           return_items_list: list = ["identifier", "name"]):
+                           filter_: dict = None, return_items: Union[list, str] = None):
     """Returns a query for querying the database for a music composition.
     Arguments:
         identifier: The identifier of the music composition in the CE.
@@ -20,12 +22,16 @@ def query_musiccomposition(identifier: str = None, title: str = None, contributo
         name: The name of the music composition.
         position: In the case that this is a movement of a larger work (e.g. a Symphony), the position of this
                   MusicComposition in the larger one.
-        return_items_list: A list of item fields that the query must return.
+        filter_: return nodes with this custom filter
+        return_items: return these items in the response
     Returns:
         The string for the quereing the music composition.
     Raises:
         UnsupportedLanguageException if the input language is not one of the supported languages.
     """
+
+    if return_items is None:
+        return_items = ["identifier", "title", "source"]
 
     if inlanguage and inlanguage not in SUPPORTED_LANGUAGES:
         raise UnsupportedLanguageException(inlanguage)
@@ -40,7 +46,9 @@ def query_musiccomposition(identifier: str = None, title: str = None, contributo
         "name": name,
         "position": position
     }
+    if filter_:
+        args["filter"] = StringConstant(make_filter(filter_))
 
     args = filter_none_args(args)
 
-    return format_query("MusicComposition", args, return_items_list)
+    return format_query("MusicComposition", args, return_items)
